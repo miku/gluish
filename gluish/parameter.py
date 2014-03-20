b732b10cc17d0022039dcfe2626cf3abed9a75d9
Martin Czygan <martin.czygan@gmail.com>
2014-03-20 17:23:37 +0100

    update docstring on ILNParameter

--- gluish/parameter.py
+++ gluish/parameter.py
@@ -12,6 +12,9 @@ Custom luigi parameters.
 import luigi
 
 class ILNParameter(luigi.Parameter):
-    """ Parse ILN, so that the result is always in the "4 char" format. """
+    """
+    Parse ILN (internal library number), so that the result is always in the
+    *4 char* format.
+    """
     def parse(self, s):
         return s.zfill(4)
