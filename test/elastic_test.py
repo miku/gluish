c6a12ab9485cb70a967a74d8110a3b11a3acc09e
Martin Czygan <martin.czygan@gmail.com>
2014-03-21 14:49:28 +0100

    run certain tests only if ES is present

--- test/elastic_test.py
+++ test/elastic_test.py
@@ -7,6 +7,7 @@ Unit test for ES indexing task. Does not require ES to run.
 from gluish.elastic import ESIndexTask
 import elasticsearch
 import unittest
+import requests
 
 
 class ESTestTask(ESIndexTask):
@@ -22,19 +23,22 @@ class ElasticTest(unittest.TestCase):
 
     def test_elastic(self):
         """ Tests w/o integration. """
+        es = elasticsearch.Elasticsearch()
         task = ESTestTask()
         self.assertEquals({}, task.settings)
         self.assertEquals({}, task.mapping)
         self.assertEquals(0, task.expected())
-        self.assertFalse(task.exists())
 
+        if es.ping():
+            self.assertFalse(task.exists())
 
-        exception_raised = False
-        try:
-            self.assertEquals(0, task.indexed())
-        except elasticsearch.NotFoundError:
-            exception_raised = True
-        self.assertTrue(exception_raised)
+        if es.ping():
+            exception_raised = False
+            try:
+                self.assertEquals(0, task.indexed())
+            except elasticsearch.NotFoundError:
+                exception_raised = True
+            self.assertTrue(exception_raised)
 
         self.assertEquals('test', task.doc_type)
         self.assertEquals('test-033261f98b7bf9b1c624908eb8b62c2f', task.index)
@@ -43,4 +47,5 @@ class ElasticTest(unittest.TestCase):
         self.assertEquals('localhost', task.hosts[0].get('host'))
         self.assertEquals(9200, task.hosts[0].get('port'))
 
-        self.assertFalse(task.complete())
+        if es.ping():
+            self.assertFalse(task.complete())
