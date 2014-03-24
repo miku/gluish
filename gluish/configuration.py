2a7029ec22db95205eb76dbc9194fcf0868abcde
Martin Czygan <martin.czygan@gmail.com>
2014-03-24 13:06:36 +0100

    added docs

--- gluish/configuration.py
+++ gluish/configuration.py
@@ -6,16 +6,17 @@ Configuration handling, adapted from luigi.
 
 from ConfigParser import ConfigParser, NoOptionError, NoSectionError
 import datetime
-import socket
 
 
 class BaseConfigParser(ConfigParser):
+    """ A ConfigParser, that can be queried for typed configuration entries. """
     NO_DEFAULT = None
     _instance = None
     _config_paths = []
 
     @classmethod
     def add_config_path(cls, path):
+        """ Append a path to read from. """
         cls._config_paths.append(path)
         cls.instance().reload()
 
@@ -24,17 +25,20 @@ class BaseConfigParser(ConfigParser):
         """ Singleton getter """
         if cls._instance is None:
             cls._instance = cls(*args, **kwargs)
-            loaded = cls._instance.reload()
+            _ = cls._instance.reload()
 
         return cls._instance
 
     def reload(self):
+        """ Reload the configuration. """
         return self._instance.read(self._config_paths)
 
-    def _get_with_default(self, method, section, option, default, expected_type=None):
-        """ Gets the value of the section/option using method. Returns default if value
-        is not found. Raises an exception if the default value is not None and doesn't match
-        the expected_type.
+    def _get_with_default(self, method, section, option, default,
+                          expected_type=None):
+        """
+        Gets the value of the section/option using method. Returns default
+        if value is not found. Raises an exception if the default value
+        is not None and doesn't match the expected_type.
         """
         try:
             return method(self, section, option)
@@ -42,28 +46,44 @@ class BaseConfigParser(ConfigParser):
             if default is BaseConfigParser.NO_DEFAULT:
                 raise
             if (expected_type is not None and default is not None and
-               	not isinstance(default, expected_type)):
+                not isinstance(default, expected_type)):
                 raise
             return default
 
     def get(self, section, option, default=NO_DEFAULT):
-        return self._get_with_default(ConfigParser.get, section, option, default)
+        """ Default. """
+        return self._get_with_default(ConfigParser.get, section, option,
+                                      default)
 
     def getboolean(self, section, option, default=NO_DEFAULT):
-        return self._get_with_default(ConfigParser.getboolean, section, option, default, bool)
+        """ Return a boolean. """
+        return self._get_with_default(ConfigParser.getboolean, section, option,
+                                      default, bool)
 
     def getint(self, section, option, default=NO_DEFAULT):
-        return self._get_with_default(ConfigParser.getint, section, option, default, int)
+        """ Return an int. """
+        return self._get_with_default(ConfigParser.getint, section, option,
+                                      default, int)
 
     def getfloat(self, section, option, default=NO_DEFAULT):
-        return self._get_with_default(ConfigParser.getfloat, section, option, default, float)
+        """ Return a float. """
+        return self._get_with_default(ConfigParser.getfloat, section, option,
+                                      default, float)
 
-    def getdate(self, section, option, default=NO_DEFAULT):
+    def getdate(self, section, option, default=NO_DEFAULT, fmt='%Y-%m-%d'):
+        """ Return a datetime.date instance. """
         value = self.get(section, option, default=default)
-        return datetime.datetime.strptime(value, self.get('core', 'date-format', '%Y-%m-%d')).date()
+        return datetime.datetime.strptime(value, fmt).date()
+
+    def getdatetime(self, section, option, default=NO_DEFAULT,
+                    fmt='%Y-%m-%d'):
+        """ Return a datetime. """
+        value = self.get(section, option, default=default)
+        return datetime.datetime.strptime(value, fmt)
 
 
 def get_config():
-    """ Convenience method (for backwards compatibility) for accessing config singleton """
+    """ Convenience method (for backwards compatibility) for accessing
+        config singleton """
     return BaseConfigParser.instance()
 
