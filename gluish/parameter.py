d064e36af4256f16729140133f3fd1b73d0b5f93
Martin Czygan <martin.czygan@gmail.com>
2014-04-29 10:33:59 +0200

    added AbstractMappedDateParameter

--- gluish/parameter.py
+++ gluish/parameter.py
@@ -9,7 +9,11 @@ Custom luigi parameters.
 
 """
 
+from luigi.parameter import MissingParameterException, _no_default
+import abc
+import datetime
 import luigi
+
 
 class ILNParameter(luigi.Parameter):
     """
@@ -18,3 +22,43 @@ class ILNParameter(luigi.Parameter):
     """
     def parse(self, s):
         return s.zfill(4)
+
+
+class AbstractMappedDateParameter(luigi.Parameter):
+    """ A date parameter, that maps certain values to others on the fly.
+    Useful, if the data source has non-standard update cycles, that can be
+    evaluated on runtime. """
+
+    @property
+    def default(self):
+	"""The default value for this Parameter.
+
+	:raises MissingParameterException: if a default is not set.
+	:return: the parsed default value.
+	"""
+	if self.__default == _no_default and self.default_from_config is None:
+	    raise MissingParameterException("No default specified")
+	if self.__default != _no_default:
+	    return self.mapped_date(self.__default)
+
+	value = self._get_default_from_config(safe=False)
+	if self.is_list:
+	    return tuple(self.parse(p.strip()) for p in value.strip().split('\n'))
+	else:
+	    return self.parse(value)
+
+    @abc.abstractmethod
+    def mapped_date(self, date):
+	""" Return the mapped date (object) for a date (object).
+
+	Example implementations:
+
+	# using another task
+	task = SomeOtherTask(date=date)
+	luigi.build([task], local_scheduler=True)
+	return task.output().open().read().strip()
+	"""
+
+    def parse(self, s):
+	date = datetime.date(*(int(v) for v in s.split('-')))
+	return self.mapped_date(date)
--- test/parameter_test.py
+++ test/parameter_test.py
@@ -1,11 +1,68 @@
 # coding: utf-8
 
-from gluish.parameter import ILNParameter
+from gluish.format import TSV
+from gluish.parameter import ILNParameter, AbstractMappedDateParameter
+from gluish.task import BaseTask
+from gluish.utils import random_string
+import datetime
+import luigi
+import tempfile
 import unittest
+
+
+class MappedDateParameter1(AbstractMappedDateParameter):
+    """ Simple static map. """
+    def mapped_date(self, date):
+	""" Map everything to a single date. """
+	return datetime.date(1970, 1, 1)
+
+
+class MappedDateParameter2(AbstractMappedDateParameter):
+    """ Actually depend on the date parameter. """
+    def mapped_date(self, date):
+	""" Map everything to a week earlier. """
+	return date - datetime.timedelta(days=7)
+
+
+class TestTask(BaseTask):
+    """ A base class for test tasks. """
+    BASE = tempfile.gettempdir()
+    TAG = 't'
+
+
+class SomeTask(TestTask):
+    """ A task that dumps a date. """
+    indicator = luigi.Parameter(default=random_string())
+    def run(self):
+	with self.output().open('w') as output:
+	    output.write_tsv(datetime.date(2000, 1, 1))
+
+    def output(self):
+	return luigi.LocalTarget(path=self.path(), format=TSV)
+
+
+class MappedDateParameter3(AbstractMappedDateParameter):
+    """ Use a SomeTask to find some date. """
+    def mapped_date(self, date):
+	""" Map everything to a week earlier. """
+	task = SomeTask()
+	luigi.build([task], local_scheduler=True)
+	value = task.output().open().next().strip()
+	return datetime.datetime.strptime(value, '%Y-%m-%d').date()
 
 
 class ParameterTest(unittest.TestCase):
 
     def test_iln_parameter(self):
-        p = ILNParameter()
-        self.assertEquals('0010', p.parse('10'))
+	param = ILNParameter()
+	self.assertEquals('0010', param.parse('10'))
+
+    def test_mapped_parameter(self):
+	param = MappedDateParameter1()
+	self.assertEquals(datetime.date(1970, 1, 1), param.parse('2014-01-01'))
+
+	param = MappedDateParameter2()
+	self.assertEquals(datetime.date(2013, 12, 25), param.parse('2014-01-01'))
+
+	param = MappedDateParameter3()
+	self.assertEquals(datetime.date(2000, 1, 1), param.parse('2014-01-01'))
