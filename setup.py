3619b7754829342d2b5a04c45a0ff6e1245f7556
Martin Czygan <martin.czygan@gmail.com>
2014-04-30 16:25:18 +0200

    v0.1.22

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 
 setup(name='gluish',
-      version='0.1.21',
+      version='0.1.22',
       description='Utils around Luigi.',
       url='https://github.com/miku/gluish',
       author='Martin Czygan',
