34b3abac995f3804474fb5170be63319db942c70
Martin Czygan <martin.czygan@gmail.com>
2014-06-19 17:29:17 +0200

    added ZeroPaddedIntParameter

--- gluish/parameter.py
+++ gluish/parameter.py
@@ -8,8 +8,23 @@ Parameter add-ons
 Custom luigi parameters.
 
 """
-# pylint: disable=F0401,C0103,R0921,E1101,W0232,R0201,R0903
+# pylint: disable=F0401,C0103,R0921,E1101,W0232,R0201,R0903,E1002
 import luigi
+
+
+class ZeroPaddedIntParameter(luigi.Parameter):
+    """
+    Parse an int, but pad. Result will be a string.
+    """
+    def __init__(self, *args, **kwargs):
+        """ Use `pad` kwarg or 4. """
+        self.pad = int(kwargs.pop('pad', 4))
+        super(ZeroPaddedIntParameter, self).__init__(*args, **kwargs)
+
+    def parse(self, s):
+        """ Parse the value and pad left with zeroes. """
+        _ = int(s)
+        return str(s).zfill(self.pad)
 
 
 class ILNParameter(luigi.Parameter):
