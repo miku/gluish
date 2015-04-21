b312e62df720f8127fe70a97af282db2097f2940
kutschkem <kutschke.michael@googlemail.com>
2015-04-21 11:19:42 +0200

    Put sqlitebck and MySQL dependency in extras section

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(name='gluish',
       package_dir={'gluish': 'gluish'},
       install_requires=[
         'BeautifulSoup==3.2.1',
-        'MySQL-python==1.2.5',
         'astroid>=1.0.1',
         'colorama==0.3.3',
         'elasticsearch==1.3.0',
@@ -36,8 +35,11 @@ setup(name='gluish',
         'pytz==2014.4',
         'requests==2.5.1',
         'six==1.9.0',
-        'sqlitebck==1.2.1',
         'urllib3==1.10',
         'wsgiref==0.1.2',
       ],
+      extras_require={
+                      'sqlitebck' : [ 'sqlitebck==1.2.1' ],
+                      'MySQL' : [ 'MySQL-python==1.2.5' ]
+                      }
 )
