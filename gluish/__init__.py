7f049dba61d3be3ddb7b3189cf098a3e6cc67558
Martin Czygan <martin.czygan@gmail.com>
2015-11-11 17:29:11 +0100

    v0.2.4

--- gluish/__init__.py
+++ gluish/__init__.py
@@ -27,7 +27,7 @@
 Gluish - few utils for luigi.
 """
 
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 from gluish.common import *
 from gluish.format import *
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ Additionally, provide some smaller utilities, like a TSV format.
 from setuptools import setup
 
 setup(name='gluish',
-      version='0.2.3',
+      version='0.2.4',
       description='Luigi helper.',
       url='https://github.com/miku/gluish',
       author='Martin Czygan',
